21aba7888d1814515b03ee961947c7e495b13920
TGedanke <TGedanke@gmail.com>
2012-02-03 13:50:45 +0000

    егор

--- fp/agent/AgOrd.php
+++ fp/agent/AgOrd.php
@@ -7,8 +7,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Заказы</title>
-    <link rel="stylesheet" type="text/css" href="../extjs/resources/css/ext-all.css"/>
-    <script type="text/javascript" src="../extjs/ext-all-debug.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://web.flippost.com/extjs/resources/css/ext-all.css"/>
+    <script type="text/javascript" src="http://web.flippost.com/extjs/ext-all-debug.js"></script>
 	<script type="text/javascript" src="Spotlight.js"></script>
 	
     <script type="text/javascript" src="agOrd.js"></script>
