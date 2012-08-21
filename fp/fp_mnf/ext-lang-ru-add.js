94ddbd3f57efd88c44d21b8d324ab2f8a95a946c
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-08-21 18:13:03 +0000

    доп русификация

---
+++ fp/fp_mnf/ext-lang-ru-add.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Russian translation additional
+ * By dvs  (utf-8 encoding)
+ * 21 August 2012
+ */
+Ext.onReady(function() {
+    Ext.override(Ext.grid.View,{
+        loadingText: 'Загрузка...'
+    });
+});
