afb5c52c2aafd9c2178e67352d54f8fcf59af0ae
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-08-23 16:35:47 +0000

    еще немного

---
+++ fp/courier/app/model/Order.js
@@ -0,0 +1,42 @@
+﻿Ext.define('Courier.model.Order', {
+	extend: 'Ext.data.Model',
+	fields: [{
+		name: 'aNo',
+		type: 'string'
+	},
+	{
+		name: 'displayNo',
+		type: 'string'
+	},
+	{
+		name: 'aCash',
+		type: 'float'
+	},
+	{
+		name: 'aAddress',
+		type: 'string'
+	},
+	{
+		name: 'client',
+		type: 'string'
+	},
+	{
+		name: 'timeB',
+		type: 'string'
+	},
+	{
+		name: 'timeE',
+		type: 'string'
+	}],
+	proxy: {
+		type: 'ajax',
+		url: 'data/data.php',
+		reader: {
+			type: 'json',
+			root: 'data'
+		},
+		extraParams: {
+			dbAct: 'getCourOrders'
+		}
+	}
+});
---
+++ fp/courier/app/model/Wb.js
@@ -0,0 +1,38 @@
+﻿Ext.define('Courier.model.Wb', {
+	extend: 'Ext.data.Model',
+	fields: [{
+		name: 'aNo',
+		type: 'string'
+	},
+	{
+		name: 'displayNo',
+		type: 'string'
+	},
+	{
+		name: 'aCash',
+		type: 'float'
+	},
+	{
+		name: 'aAddress',
+		type: 'string'
+	},
+	{
+		name: 'client',
+		type: 'string'
+	},
+	{
+		name: 'tdd',
+		type: 'string'
+	}],
+	proxy: {
+		type: 'ajax',
+		url: 'data/data.php',
+		reader: {
+			type: 'json',
+			root: 'data'
+		},
+		extraParams: {
+			dbAct: 'getCourWbs'
+		}
+	}
+});
