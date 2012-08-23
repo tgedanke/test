048998cebed609ec112bf48dee0f60017fb6a315
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-08-23 19:05:50 +0000

    порядок закладок

--- fp/fp_mnf/app/view/mainform/MainPanel.js
+++ fp/fp_mnf/app/view/mainform/MainPanel.js
@@ -2,20 +2,19 @@ Ext.define('FpMnf.view.mainform.MainPanel', {
 	extend : 'Ext.tab.Panel',
 	alias : 'widget.mainpanel',
 	requires : ['FpMnf.view.mainform.MnfPanel', 'FpMnf.view.orders.OrdGrid', 'FpMnf.view.orders.OrdWin'],
-	activeItem : 0,
+	activeTab : 1,
 	margins : '5 5 5 5',
 	initComponent : function () {
 		Ext.apply(this, {
 			items : [{
-					xtype : 'mnfpanel',
-					title : 'Манифесты'
-				}, {
-				
 					title : 'Накладные'
-					
 				}, {
 					xtype : 'ordgrid',
 					title : 'Заказы'
+				},
+                {
+					xtype : 'mnfpanel',
+					title : 'Манифесты'
 				}
 			]
 		});
