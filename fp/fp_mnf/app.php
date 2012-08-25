7a0dc9cf2bcaa33812fdddbc5a639d30dee5a633
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-08-25 10:04:38 +0000

    и даже вот так

--- fp/fp_mnf/app.php
+++ fp/fp_mnf/app.php
@@ -4,18 +4,26 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>ФлипПост WEB</title>
-    <link rel="stylesheet" type="text/css" href="../../extjs-4.1.1/resources/css/ext-all.css"/>
-    <!--script type="text/javascript" src="../../extjs-4.1.1/ext-debug.js"></script>
-	<script type="text/javascript" src="../../extjs-4.1.1/locale/ext-lang-ru.js"></script>
-	<script type="text/javascript" src="ext-lang-ru-add.js"></script-->
+    <link rel="stylesheet" type="text/css" href="/extjs-4.1.1/resources/css/ext-all.css"/>
 
-    <!--script type="text/javascript" src="../../extjs-4.1.1/ext-all.js"></script>
+	<!-- dev mode -->
+    <!--script type="text/javascript" src="/extjs-4.1.1/ext-debug.js"></script>
+	<script type="text/javascript" src="/extjs-4.1.1/locale/ext-lang-ru.js"></script>
+	<script type="text/javascript" src="ext-lang-ru-add.js"></script>
+	<script type="text/javascript" src="app.js"></script-->
+
+    <!-- prod mode -->
+	<script type="text/javascript" src="/extjs-4.1.1/ext-all.js"></script>
+	<script type="text/javascript" src="/extjs-4.1.1/locale/ext-lang-ru.js"></script>
+	<script type="text/javascript" src="ext-lang-ru-add.js"></script>
+    <script type="text/javascript" src="all-classes.js"></script>
 	<script type="text/javascript" src="app.js"></script>
-    <script type="text/javascript" src="all-classes.js"></script-->
 
-
-    <script type="text/javascript" src="/extjs-4.1.1/ext.js"></script>
-    <script type="text/javascript" src="app-all.js"></script>
+	<!-- prod mode mini -->
+    <!--script type="text/javascript" src="/extjs-4.1.1/ext.js"></script>
+	<script type="text/javascript" src="/extjs-4.1.1/locale/ext-lang-ru.js"></script>
+	<script type="text/javascript" src="ext-lang-ru-add.js"></script>
+    <script type="text/javascript" src="app-all.js"></script-->
 
 
 </head>
