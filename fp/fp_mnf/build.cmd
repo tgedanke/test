18ecc7ce10c81bbb9468f3e14a4b0135e7cf0721
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-11-14 17:49:19 +0000

    +1

--- fp/fp_mnf/build.cmd
+++ fp/fp_mnf/build.cmd
@@ -1,1 +1,1 @@
-sencha create jsb -a http://test2.dvs/fp/fp_mnf/app.html -p app.jsb3 -v
+"C:\Program Files\SenchaSDKTools-2.0.0-beta3\sencha" create jsb -a http://localhost/fp_mnf/app.html -p app.jsb3 -v
