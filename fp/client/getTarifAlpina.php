c07e5f8d9491dd7a38e592fef21c00541166ae3c
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-12-10 12:12:02 +0000

    калькулятор тарифа для http://www.alpinabook.ru/

---
+++ fp/client/getTarifAlpina.php
@@ -0,0 +1,48 @@
+<?php
+//калькулятор тарифа для http://www.alpinabook.ru/
+
+error_reporting(0);
+header("Content-type: text/plain; charset=utf-8");
+
+$errorMsg = 'ERROR|||';
+
+//проверка параметров
+$city = $_REQUEST['loc'];
+if(!$city) exit($errorMsg . 'no city' . '|||');
+//$city = iconv("UTF-8", "windows-1251", $city);
+
+$weight = $_REQUEST['totalweight'];
+if(!$weight) exit($errorMsg . 'no weight' . '|||');
+
+include "dbConnect.php";
+//поиск кода города
+$query = "exec wwwGetCityCodes @pName = '{$city}, Россия'";
+$query = iconv("UTF-8", "windows-1251", $query);
+
+$result = mssql_query($query);
+if(!$result) exit($errorMsg . iconv("windows-1251", "UTF-8", mssql_get_last_message()) . '|||');
+if(mssql_num_rows($result) != 1) exit($errorMsg . 'city not found ' . mssql_num_rows($result) . '|||');
+
+$row = mssql_fetch_array($result, MSSQL_ASSOC);
+$code = $row['Code'];
+
+//получаем тариф
+$weight = $weight / 1000; //вес в кг
+
+$query = "exec wwwGetTarif @code = '{$code}', @wt = {$weight}";
+$query = iconv("UTF-8", "windows-1251", $query);
+//exit($query);
+$result = mssql_query($query);
+if(!$result) exit($errorMsg . iconv("windows-1251", "UTF-8", mssql_get_last_message()) . '|||');
+if(mssql_num_rows($result) == 0) exit($errorMsg . 'tarif not found ' . '|||');
+
+$row = mssql_fetch_array($result, MSSQL_ASSOC);
+$tarif = $row['price'];
+$tarif = $tarif * 1.18; //НДС
+$tarif = $tarif * 1.05; //топливная надбавка
+$tarif = round($tarif, 2);
+//exit($tarif);
+
+//вот и все
+exit("SUCCESS|||{$tarif}|||");
+?>
