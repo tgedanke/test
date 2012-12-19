2a2dbf25ac2f964dab1cc699d4bfbc30c4eaa426
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2012-12-19 08:18:59 +0000

    странные они

--- fp/client/getTarifAlpina.php
+++ fp/client/getTarifAlpina.php
@@ -4,15 +4,15 @@
 error_reporting(0);
 header("Content-type: text/plain; charset=utf-8");
 
-$errorMsg = 'ERROR|||';
+$errorMsg = 'SUCCESS||||||';
 
 //проверка параметров
 $city = $_REQUEST['loc'];
-if(!$city) exit($errorMsg . 'no city' . '|||');
+if(!$city) exit($errorMsg . 'no city');
 //$city = iconv("UTF-8", "windows-1251", $city);
 
 $weight = $_REQUEST['totalweight'];
-if(!$weight) exit($errorMsg . 'no weight' . '|||');
+if(!$weight) exit($errorMsg . 'no weight');
 
 include "dbConnect.php";
 //поиск кода города
@@ -21,7 +21,7 @@ $query = iconv("UTF-8", "windows-1251", $query);
 
 $result = mssql_query($query);
 if(!$result) exit($errorMsg . iconv("windows-1251", "UTF-8", mssql_get_last_message()) . '|||');
-if(mssql_num_rows($result) != 1) exit($errorMsg . 'city not found ' . mssql_num_rows($result) . '|||');
+if(mssql_num_rows($result) != 1) exit($errorMsg . "Служба не имеет доставки в город {$city} ");
 
 $row = mssql_fetch_array($result, MSSQL_ASSOC);
 $code = $row['Code'];
@@ -33,8 +33,8 @@ $query = "exec wwwGetTarif @code = '{$code}', @wt = {$weight}";
 $query = iconv("UTF-8", "windows-1251", $query);
 //exit($query);
 $result = mssql_query($query);
-if(!$result) exit($errorMsg . iconv("windows-1251", "UTF-8", mssql_get_last_message()) . '|||');
-if(mssql_num_rows($result) == 0) exit($errorMsg . 'tarif not found ' . '|||');
+if(!$result) exit($errorMsg . iconv("windows-1251", "UTF-8", mssql_get_last_message()));
+if(mssql_num_rows($result) == 0) exit($errorMsg . 'tarif not found ');
 
 $row = mssql_fetch_array($result, MSSQL_ASSOC);
 $tarif = $row['price'];
