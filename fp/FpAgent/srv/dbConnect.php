249828442d60bdfa21b5f3087050062d87a9c925
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2013-04-03 15:25:11 +0000

    настройки sql-сессии, чтобы возвращались ошибки

--- fp/FpAgent/srv/dbConnect.php
+++ fp/FpAgent/srv/dbConnect.php
@@ -6,5 +6,10 @@ mssql_connect(".", "pod", "");
 //mssql_connect("localhost", "dvs", "");
 //mssql_connect("ROCKET", "pod", "ytabupfgznstcnfdbnm");
 mssql_select_db("alert_f");
-
+mssql_query("set quoted_identifier on
+set arithabort on
+set numeric_roundabort off
+set ansi_warnings on
+set ansi_padding on
+set ansi_nulls on");
 ?>
