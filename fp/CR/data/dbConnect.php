cea1aaf53708a34bd3d07d7940a134e23a5e9ece
DaViSx13 <DaViSx13@097f7491-7637-a856-7271-a34200ca55b7>
2013-04-10 12:03:40 +0000

    настройка sql-соединения

--- fp/CR/data/dbConnect.php
+++ fp/CR/data/dbConnect.php
@@ -4,4 +4,10 @@ ini_set("mssql.datetimeconvert", 0);
 mssql_connect(".", "dvs", "");
 //mssql_connect("ROCKET", "pod", "ytabupfgznstcnfdbnm");
 mssql_select_db("alert_f");
+mssql_query("set quoted_identifier on
+set arithabort on
+set numeric_roundabort off
+set ansi_warnings on
+set ansi_padding on
+set ansi_nulls on");
 ?>
